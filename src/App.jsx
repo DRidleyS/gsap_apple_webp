a11220ee761cea46269b2a55aa56125970da994e
DRidleyS <dorianridley@gmail.com>
2025-11-20 14:20:17 -0800

    Showcase section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import Hero from "./components/Hero.jsx";
 import NavBar from "./components/NavBar.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
+import Showcase from "./components/Showcase.jsx";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Hero />
+      <ProductViewer />
+      <Showcase />
     </main>
   );
 };
