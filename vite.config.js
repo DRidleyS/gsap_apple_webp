08c323874d216184f68c844ae5c285e1128624a6
DRidleyS <dorianridley@gmail.com>
2025-11-21 17:06:01 -0800

    Fix local dev and GitHub Pages deployment issues

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/gsap_apple_webp/", // Replace with your repository name
+  base: process.env.NODE_ENV === "production" ? "/gsap_apple_webp/" : "/",
 });
